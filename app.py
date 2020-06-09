570fef4d1eb81eb5951135fb22b09c143b339bb3
Aryan470 <aryankhatri2012@gmail.com>
2020-06-08 23:53:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from socraticos import socketio, create_app
+import os
 
 if __name__ == "__main__":
-    socketio.run(create_app(), debug=True)
+    port = os.environ["PORT"]
+    if not port:
+        port = 5000
+    socketio.run(create_app(), debug=True, port=port)
