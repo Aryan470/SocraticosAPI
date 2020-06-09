ba168a19b3dffa289250a259171ca0690ba34347
Aryan470 <aryankhatri2012@gmail.com>
2020-06-09 00:32:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from socraticos import socketio, create_app
 import os
 
-gapp = create_app()
+port = os.environ["PORT"]
+if not port:
+  port = 7000
+
+socketio.run(create_app(), port=port, debug=True)
