7262cb9d216e4c5e7e16217bb74cb492e4efea38
Aryan470 <aryankhatri2012@gmail.com>
2020-06-12 22:55:41 +0000

    Added JSON display for requests
    Fixes Aryan470/SocraticosAPI#39

--- socraticos/__init__.py
+++ socraticos/__init__.py
@@ -8,7 +8,7 @@ cred = credentials.Certificate(json.loads(os.environ["PROJECT_AUTH"]))
 firebase_admin.initialize_app(cred)
 fireClient = firestore.client()
 
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, session, redirect, request
 from flask_socketio import SocketIO
 
 socketio = SocketIO(logger=True)
@@ -24,6 +24,10 @@ def create_app():
     app.register_blueprint(users.users, url_prefix="/users")
     app.register_blueprint(groups.groups, url_prefix="/groups")
     app.register_blueprint(auth.auth, url_prefix="/auth")
+
+    @app.before_request
+    def log_request_info():
+        app.logger.debug('JSON: %s', request.get_json())
 
     # Redirect to API documentation
     @app.route("/")
