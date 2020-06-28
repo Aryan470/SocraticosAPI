f1571246f7b30ffe79ba141d0c190472161ca869
Aryan470 <aryankhatri2012@gmail.com>
2020-06-28 14:45:05 -0500

    Update __init__.py

--- socraticos/__init__.py
+++ socraticos/__init__.py
@@ -24,7 +24,7 @@ def create_app():
         app.secret_key = os.environ["SECRET_KEY"]
     else:
         app.secret_key = "DEVELOPMENT"
-    CORS(app)
+    CORS(app, supports_credentials=True)
     app.register_blueprint(users.users, url_prefix="/users")
     app.register_blueprint(groups.groups, url_prefix="/groups")
     app.register_blueprint(auth.auth, url_prefix="/auth")
