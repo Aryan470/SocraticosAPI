d3ee413bd81b62717bc7b2b097ab28027b6ffc02
Aryan470 <aryankhatri2012@gmail.com>
2020-07-01 16:30:51 -0500

    Only adds encoded session for 200 responses

--- socraticos/__init__.py
+++ socraticos/__init__.py
@@ -44,11 +44,12 @@ def create_app():
 
     @app.after_request
     def encodeSession(response):
-        body_data = response.get_json()
-        session_data = {}
-        for key in session:
-            session_data[key] = session[key]
-        return make_response({"content": body_data, "session": jws.sign(session_data, app.secret_key, algorithm='HS256')})
+        if response.status == "200":
+            body_data = response.get_json()
+            session_data = {}
+            for key in session:
+                session_data[key] = session[key]
+            return make_response({"content": body_data, "session": jws.sign(session_data, app.secret_key, algorithm='HS256')})
 
 
     # Redirect to API documentation
