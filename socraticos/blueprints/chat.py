ea158c8becfb3e4bc2a92f0a106e63bc91d3ed94
Aryan470 <aryankhatri2012@gmail.com>
2020-07-02 15:05:26 -0500

    Update chat.py

--- socraticos/blueprints/chat.py
+++ socraticos/blueprints/chat.py
@@ -79,7 +79,7 @@ def pinMessage(messageID: str, authorID: str, groupID: str, unpin: bool = False)
     if not group_obj.exists:
         raise FileNotFoundError("Group does not exist")
 
-    if authorID not in group_obj.get("mentors") and not in group_obj.get("students"):
+    if authorID not in group_obj.get("mentors") and authorID not in group_obj.get("students"):
         raise PermissionError("Must be mentor or student to pin messages")
 
     msg_ref = group_ref.collection("chatHistory").document(messageID)
